96cd8a4bb299663e54aaa57ec396ba82898f1513
unknown <mdomarfaruk149518@gmail.com>
2023-09-27 00:51:18 +0600

    first commit

---
+++ src/app/(withLayout)/layout.tsx
@@ -0,0 +1,16 @@
+import Contents from "@/components/ui/Contents";
+import Sidebar from "@/components/ui/Sidebar";
+import { Layout } from "antd";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Layout hasSider>
+      <Sidebar />
+      <Layout>
+        <Contents>{children}</Contents>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default DashboardLayout;
